8d816a9fad1ba3df9524139c719076b294a94069
Unknown <Den.lavrov.95@mail.ru>
2018-10-12 12:28:09 +0300

    fix port

--- nodeBot.js
+++ nodeBot.js
@@ -8,12 +8,12 @@ const tg = new Telegram.Telegram('646372471:AAGeB5J7Dw2Ih3zY_Kne94bfz4SZUPnDYf8'
     workers: 1,
     webhook: {
         url: 'https://cryptic-shelf-51638.herokuapp.com/',
-        port: '3000',
-        host: '0.0.0.0'
+        port: 8888,
+        host: 'localhost'
     },
     webAdmin: {
         port: 8888,
-        host: '0.0.0.0'
+        host: 'localhost'
     }
 })
 
