1681196988753f2ac1d2852e3620655e3f7a2748
Unknown <Den.lavrov.95@mail.ru>
2018-10-12 16:35:59 +0300

    12

--- nodeBot.js
+++ nodeBot.js
@@ -11,11 +11,11 @@ const tg = new Telegram.Telegram('646372471:AAGeB5J7Dw2Ih3zY_Kne94bfz4SZUPnDYf8'
         port: process.env.PORT || 8080,
         host: process.env.WEBHOST || 'cryptic-shelf-51638.herokuapp.com'
     },
-    webAdmin: {
-        port: 8080,
-        host: 'cryptic-shelf-51638.herokuapp.com'
+    // webAdmin: {
+    //     port: 8080,
+    //     host: 'cryptic-shelf-51638.herokuapp.com'
 
-    }
+}
 })
 
 class PingController extends TelegramBaseController {
