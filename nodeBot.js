c96b28db6e0ef492fe69eb1e88663f4f52b2e6b8
Unknown <Den.lavrov.95@mail.ru>
2018-10-12 12:37:43 +0300

    host

--- nodeBot.js
+++ nodeBot.js
@@ -9,11 +9,11 @@ const tg = new Telegram.Telegram('646372471:AAGeB5J7Dw2Ih3zY_Kne94bfz4SZUPnDYf8'
     webhook: {
         url: 'https://cryptic-shelf-51638.herokuapp.com/',
         port: 8888,
-        host: 'localhost'
+        host: 'cryptic-shelf-51638.herokuapp.com'
     },
     webAdmin: {
         port: 8888,
-        host: 'localhost'
+        host: 'cryptic-shelf-51638.herokuapp.com'
     }
 })
 
