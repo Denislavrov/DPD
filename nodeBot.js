26a7480f66db01fca2a45a640d110b8d22278731
Unknown <Den.lavrov.95@mail.ru>
2018-10-12 16:33:39 +0300

    may be?!

--- nodeBot.js
+++ nodeBot.js
@@ -7,9 +7,9 @@ const TextCommand = Telegram.TextCommand
 const tg = new Telegram.Telegram('646372471:AAGeB5J7Dw2Ih3zY_Kne94bfz4SZUPnDYf8', {
     workers: 1,
     webhook: {
-        url: 'https://cryptic-shelf-51638.herokuapp.com',
-        port: 8080,
-        host: 'cryptic-shelf-51638.herokuapp.com'
+        url: process.env.WEBHOOK || 'https://cryptic-shelf-51638.herokuapp.com',
+        port: process.env.PORT || 8080,
+        host: process.env.WEBHOST || 'cryptic-shelf-51638.herokuapp.com'
     },
     webAdmin: {
         port: 8080,
