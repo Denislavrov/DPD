6ce412c21716e32c5696b52fb6b190f412a2bb99
Unknown <Den.lavrov.95@mail.ru>
2018-10-29 15:36:34 +0300

    test-test

--- nodeBot.js
+++ nodeBot.js
@@ -6,44 +6,17 @@ const TelegramBaseController = Telegram.TelegramBaseController
 const TextCommand = Telegram.TextCommand
 const tg = new Telegram.Telegram('646372471:AAGeB5J7Dw2Ih3zY_Kne94bfz4SZUPnDYf8');
 
-class StartController extends TelegramBaseController {
-    /**
-     * @param {Scope} $ 
-     */
-    start($) {
-        $.sendMessage('Hello!')
-    }
-
-    get routes() {
-        return {
-            'startHandler': 'start'
-        }
+class OtherwiseController extends TelegramBaseController {
+    handle() {
+        console.log('otherwise')
     }
 }
 
-class StopController extends TelegramBaseController {
-    /**
-     * @param {Scope} $ 
-     */
-    stop($) {
-        $.sendMessage('fuck you!')
-    }
-
-    get routes() {
-        return {
-            'stopHandler': 'stop'
-        }
-    }
-}
 
 
 
 tg.router
-    .when(
-        new TextCommand('/start', 'startHandler'),
-        new StartController()
-    )
-    .when(
-        new TextCommand('stop', 'stopHandler'),
-        new StopController()
-    )
+    .when(new TextCommand('/start', 'startCommand'), new StartController())
+    .when(new TextCommand('/stop', 'stopCommand'), new StopController())
+    .when(new TextCommand('/restart', 'restartCommand'), new RestartController())
+    .otherwise(new OtherwiseController())
