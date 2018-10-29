7f20682b094e64432c9f2bfbbcaa5676670cb8e4
Unknown <Den.lavrov.95@mail.ru>
2018-10-29 15:48:36 +0300

    base test

--- nodeBot.js
+++ nodeBot.js
@@ -6,34 +6,23 @@ const TelegramBaseController = Telegram.TelegramBaseController
 const TextCommand = Telegram.TextCommand
 const tg = new Telegram.Telegram('646372471:AAGeB5J7Dw2Ih3zY_Kne94bfz4SZUPnDYf8');
 
-class OtherwiseController extends TelegramBaseController {
-    handle() {
-        $.sendMessage('otherwise')
-    }
-}
-
-class StartConstoller extends TelegramBaseController {
+class PingController extends TelegramBaseController {
     /**
      * @param {Scope} $ 
      */
-    start($) {
-        $.sendMessage('Hello!')
+    pingHandler($) {
+        $.sendMessage('pong')
     }
 
     get routes() {
         return {
-            'startCommand': 'start'
+            'pingCommand': 'pingHandler'
         }
     }
 }
 
-
-
-
-
-
 tg.router
-    .when(new TextCommand('/start', 'startCommand'), new StartController())
-    .when(new TextCommand('/stop', 'stopCommand'), new StopController())
-    .when(new TextCommand('/restart', 'restartCommand'), new RestartController())
-    .otherwise(new OtherwiseController())
+    .when(
+        new TextCommand('ping', 'pingCommand'),
+        new PingController()
+    )
